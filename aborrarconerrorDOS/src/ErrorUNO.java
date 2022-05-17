36aa11ed2b5ff383b3edd0b8103d355caf744f93
Juan Jesus Lozano <garbagetin1@gmail.com>
2022-05-17 12:02:02 +0200

    segundo project con error

---
+++ aborrarconerrorDOS/src/ErrorUNO.java
@@ -0,0 +1,16 @@
+
+
+public class ErrorUNO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("cada vez tengo menos tiempo, y mas cosas que hacer");
+		System.out.println("seguimos en la brecha");
+
+	}
+
+}
+
+
+
+ tienes que borrar esto si quieres quitar el error
