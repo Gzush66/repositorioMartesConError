36d3c82d2675c2b731315ba15e0008033b120cfc
Juan Jesus <105355709+juanjesusDAW@users.noreply.github.com>
2022-05-17 13:33:37 +0200

    Update ErrorUNO.java
    las capturas  has de hacerlas

--- aborrarconerrorDOS/src/ErrorUNO.java
+++ aborrarconerrorDOS/src/ErrorUNO.java
@@ -7,6 +7,11 @@ public class ErrorUNO {
 		System.out.println("cada vez tengo menos tiempo, y mas cosas que hacer");
 		System.out.println("seguimos en la brecha");
 	
+		
+		
+		
+		
+		systme.out.println(" jesus , ya te lo he arreglado dos veces, y no has hecho las capturas, aclarate"");
 	}
 
 }
