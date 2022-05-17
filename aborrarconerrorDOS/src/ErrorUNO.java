fa92d19afa61fcb37e73220763920a3cc2427acb
Juan Jesus Lozano <garbagetin1@gmail.com>
2022-05-17 13:28:29 +0200

    variacion

--- aborrarconerrorDOS/src/ErrorUNO.java
+++ aborrarconerrorDOS/src/ErrorUNO.java
@@ -6,6 +6,8 @@ public class ErrorUNO {
 		// TODO Auto-generated method stub
 		System.out.println("cada vez tengo menos tiempo, y mas cosas que hacer");
 		System.out.println("seguimos en la brecha");
+		
+		jlkjjdd
 
 	}
 
